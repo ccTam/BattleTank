012a568755fefadfdd20bd2b78f3aa143ccb69da
Chi Tam <chi.tam.377@my.csun.edu>
2017-02-14 01:17:15 -0800

    Lec116

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -3,6 +3,17 @@
 #include "BattleTank.h"
 #include "TankPlayerController.h"
 
+void ATankPlayerController::BeginPlay() {
+	Super::BeginPlay();
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank) {
+		UE_LOG(LogTemp, Warning, TEXT("Player Controller not possesing a tank"));
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("Player Controller possesing: %s"), *(ControlledTank->GetName()));
+	}
+}
+
 ATank* ATankPlayerController::GetControlledTank() const {
 	return Cast<ATank> (GetPawn());
 }
--- BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -15,7 +15,5 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 	GENERATED_BODY()
 public:
 	ATank* GetControlledTank() const;
-	
-	
-	
+	void BeginPlay() override;
 };
